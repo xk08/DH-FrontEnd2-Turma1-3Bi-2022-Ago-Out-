65730be1a6971fcb8973eb6b24d7c55d87cce100
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-09-03 20:29:55 -0300

    Arquivos base para aula 16 sobre assincronismo e promisses

---
+++ Aula16--IntroducaoAoAssincronismo/Aula/js/index.js
@@ -0,0 +1,12 @@
+/* Nesta aula vamos "simular" requisições assincronas para entender o conceito de promisses no JS */
+
+
+
+
+/* Criando uma nova promisse */
+
+
+
+
+
+/* Resolvendo/Utilizando uma promisse */
