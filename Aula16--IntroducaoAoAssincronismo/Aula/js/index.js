c92928eba98ceaf1a8de450c2e8398e14fa59870
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-09-05 20:34:26 -0300

    Aula 16, dia 05/09/22

--- Aula16--IntroducaoAoAssincronismo/Aula/js/index.js
+++ Aula16--IntroducaoAoAssincronismo/Aula/js/index.js
@@ -1,12 +1,53 @@
 /* Nesta aula vamos "simular" requisições assincronas para entender o conceito de promisses no JS */
 
+/* Criando um novo objeto JS pro usuário */
+let objetoUsuario = {
+    login: "marcos@dh.com.br",
+    senha: "12345"
+}
 
+/* Convertando o objeto para JSON */
+let objetoUsuarioJson = JSON.stringify(objetoUsuario);
 
 
 /* Criando uma nova promisse */
+let validacao = true;
 
+/* Estrutura de callbacks que recebe 2 funções */
+let novaPromisse = new Promise(function (ressolve, reject) {
 
+    setTimeout(() => { //Simulando que a requisição demorou
+        if (validacao) {
+            /* Executa a função no bloco then() e passa o resultado para seu retorno */
+            ressolve(objetoUsuarioJson);
+        } else {
+            /* Executa a função no bloco catch() e passa o resultado para seu retorno */
+            reject("A requisição teve algum problema");
+        }
+    }, 2500);
 
+});
 
+/* Resolvendo a promisse */
+novaPromisse.then(
+    function (resultado) {
+        console.log("Primeiro Then");
+        console.log(resultado);
 
-/* Resolvendo/Utilizando uma promisse */
+        /* if(true){
+            throw "Deu ruim na requisição!";
+        } */
+        return JSON.parse(resultado);
+    }
+).then(
+    function (resultado) {
+        console.log("Segundo Then");
+        console.log(resultado);
+    }
+)
+
+    .catch(
+        function (erro) {
+            console.log(erro);
+        }
+    );
